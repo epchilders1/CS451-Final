f0ac00ec715e929414a75bd57dfdfaa5ec3d3ddb
epchilders1 <echilders2004@gmail.com>
2025-12-01 12:45:08 -0600

    basic netflix top 10 web scraping

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,69 @@
-
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+from flask import Flask, render_template, jsonify
+import logging
 
-def scrape_website(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup.title.string
-    else:
-        return None
+kaggle_dataset = "ashishkumarak/netflix-reviews-playstore-daily-updated"
+top10_url="https://www.netflix.com/tudum/top10"
+
+date = datetime.now().strftime("%Y-%m-%d")
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+class NetflixDataCollector:
+    
+    def __init__(self):
+        
+        self.kaggle_dataset = kaggle_dataset
+        self.top10_url = top10_url
+        self.api = None
+        self._init_kaggle_api()
+
+    def _init_kaggle_api(self):
+        try:
+            self.api = KaggleApi()
+            self.api.authenticate()
+            logger.info("✓ Kaggle API authenticated successfully")
+        except Exception as e:
+            logger.error(f"✗ Failed to authenticate Kaggle API: {e}")
+            logger.error("Make sure kaggle.json is in ~/.kaggle/ directory")
+            raise
+    def scrape_top10(self):
+        try:
+            response = requests.get(top10_url)
+            html_content = response.text
+
+            print("Received response:", response.status_code)
+
+            soup = BeautifulSoup(html_content, 'html.parser')
+            title_list = soup.find_all('td', class_="title", attrs={'data-uia': 'top10-table-row-title'})
+            for title in title_list:
+                movie_title = title.find("button")
+                print(movie_title.text.strip())
+            print(date)
+
+        except Exception as e:
+            logger.error(f"Error during web scraping: {e}")
+            return []
+
+    def weekly_update(self):
+        self.scrape_top10()
+    
+
+
 
 def main():
     print("Hello, World!")
+    pipeline = NetflixDataCollector()
+    pipeline.weekly_update()
 
 
 if __name__ == "__main__":
