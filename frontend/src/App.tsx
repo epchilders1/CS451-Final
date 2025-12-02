121b7d74159108c9cd5df1a07e7525c37b6017af
epchilders1 <echilders2004@gmail.com>
2025-12-02 13:31:57 -0600

    fixed root path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/CS451-Final" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
